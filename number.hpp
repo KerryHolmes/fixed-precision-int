583d176d7625414a25b0022512d2491531c15809
KerryHolmes <kjh80@zips.uakron.edu>
2016-09-24 22:57:56 -0400

    1
    Skeleton of number add with default constructor.

--- number.hpp
+++ number.hpp
@@ -1,1 +1,29 @@
+/***********************************************************************
+  * Project 1: Fixed Precision Binary integer
+  *
+  * Author: Kerry Holmes
+  *         kjh80@zips.uakron.edu
+  *
+  * Purpose: TODO Write the purpose
+  *********************************************************************/
+#include <cassert>
+#include <iostream>
+#include <vector>
 
+#ifndef NUMBER_HPP
+#define NUMBER_HPP
+
+struct number{
+
+  private:
+  
+  std::vector<int> digits;
+  
+  public:
+  number()
+  :digits(2048,0)
+  {}
+
+};
+
+#endif
