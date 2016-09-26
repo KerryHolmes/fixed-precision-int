a8e243d26fa819fbf9ee399f371d6850cfab8bf7
KerryHolmes <kjh80@zips.uakron.edu>
2016-09-25 22:57:41 -0400

    Add comparison operators and the outline of addition

--- number.hpp
+++ number.hpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <algorithm>
 
 #ifndef NUMBER_HPP
 #define NUMBER_HPP
@@ -28,7 +29,7 @@ class Number{
   {}
 
   Number(int decimal)
-  :digits(2048)
+  :digits(2048,0)
   {
      int mst_sig_bit = std::floor(std::log2(decimal));
      for( int i = 0; i <= mst_sig_bit; ++i)
@@ -44,8 +45,74 @@ class Number{
     return *this;
   }
 
+  Number& operator=( int decimal)
+  {
+    digits = std::vector<int>(2048,0);
+    int mst_sig_bit = std::floor(std::log2(decimal));
+     for( int i = 0; i <= mst_sig_bit; ++i)
+     {
+        digits[i] = decimal % 2;
+        decimal = decimal / 2;
+     }
+     return *this;
+  }
 
-
+  Number operator+( const Number& num)
+  {
+    Number sum(*this);
+    for(int i = 0; i < 2048; ++i)
+    {
+      add_binary(sum, num);
+    }
+    return sum;
+  }
+ 
+ friend bool operator==( const Number& lhs, const Number& rhs);
+ friend bool operator<( const Number& lhs, const Number& rhs);
+ friend bool operator>( const Number& lhs, const Number& rhs);
+ friend int add_binary( const Number& lhs, const Number& rhs);
 };
+
+bool operator==( const Number& lhs, const Number& rhs)
+{
+    for( int i = 2047; i >= 0; i++)
+    {
+      if(lhs.digits[i] != rhs.digits[i])
+        return false;
+    }
+    return true;
+}
+
+bool operator!=( const Number& lhs, const Number& rhs)
+{
+  return !(lhs == rhs);
+}
+
+bool operator<( const Number& lhs, const Number& rhs)
+{
+    return std::lexicographical_compare(lhs.digits.begin(), lhs.digits.end(),
+                                        rhs.digits.begin(), rhs.digits.end());
+}
+
+bool operator>( const Number& lhs, const Number& rhs)
+{
+    return std::lexicographical_compare(rhs.digits.begin(), rhs.digits.end(),
+                                        lhs.digits.begin(), lhs.digits.end());
+}
+
+bool operator<=( const Number& lhs, const Number& rhs)
+{
+    return !(lhs > rhs);
+}
+
+bool operator>=( const Number& lhs, const Number& rhs)
+{
+    return !(lhs < rhs);
+}
+
+int add_binary( const Number& lhs, const Number& rhs)
+{
+  return 0;//TODO write this
+}
 
 #endif
