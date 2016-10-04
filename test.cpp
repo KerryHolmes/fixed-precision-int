d6faf2f3d7906de43d12397a8d279bdd205bc779
KerryHolmes <kjh80@zips.uakron.edu>
2016-10-04 19:45:30 -0400

    change test file

--- test.cpp
+++ test.cpp
@@ -5,7 +5,7 @@ int main(int argc, char ** argv)
 {
     Number test();
     test = 100;
-    Number test2(test);
-    std::cout << test == test2;
+    Number test2(&test);
+    std::cout << (test == test2);
     return 0;
 }
