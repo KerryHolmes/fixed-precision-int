4396fbf9e74a468da42e62b8a81cee5528366378
KerryHolmes <kjh80@zips.uakron.edu>
2016-10-04 20:54:07 -0400

    Add tests to test.cpp

--- test.cpp
+++ test.cpp
@@ -6,7 +6,12 @@ int main(int argc, char ** argv)
 {
     Number test;
     test = 100;
-    Number test2(test);
-    std::cout << (test == test2);
+    Number test2 = test;
+    std::cout.setf(std::ios::boolalpha);
+   std::cout<< test.convert_decimal()<<std::endl;
+    std::cout<< test2.convert_decimal()<<std::endl;
+//std::cout << static_cast<bool>(test == test)<<std::endl;
+    std::cout << static_cast<bool>(test == 100)<<std::endl;
+    std::cout << static_cast<bool>(test != test2)<<std::endl;
     return 0;
 }
