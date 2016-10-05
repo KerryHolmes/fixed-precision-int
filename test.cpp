12f06299a047c26592b96aca9ca5ec13223ade94
KerryHolmes <kjh80@zips.uakron.edu>
2016-10-04 20:58:02 -0400

    add more tests to test.cpp

--- test.cpp
+++ test.cpp
@@ -8,10 +8,16 @@ int main(int argc, char ** argv)
     test = 100;
     Number test2 = test;
     std::cout.setf(std::ios::boolalpha);
-   std::cout<< test.convert_decimal()<<std::endl;
+    std::cout<< test.convert_decimal()<<std::endl;
     std::cout<< test2.convert_decimal()<<std::endl;
-//std::cout << static_cast<bool>(test == test)<<std::endl;
+    std::cout << static_cast<bool>(test == test)<<std::endl;
     std::cout << static_cast<bool>(test == 100)<<std::endl;
     std::cout << static_cast<bool>(test != test2)<<std::endl;
+    std::cout << static_cast<bool>(test > test2)<<std::endl;
+    std::cout << static_cast<bool>(test > test2)<<std::endl;
+    test += test2;
+    std::cout<< test.convert_decimal()<<std::endl;
+
+
     return 0;
 }
